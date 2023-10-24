ce9cb8ed8a932d99a058d03bb6c04d256b2de282
Isabela <belarbarbosa@gmail.com>
2023-10-24 17:22:58 -0300

    Melhorando respostas

--- Exemplo Avaliacao/resolucao.py
+++ Exemplo Avaliacao/resolucao.py
@@ -6,40 +6,32 @@ dataset = np.loadtxt('arquivos/paises.csv', delimiter=';', dtype=str, encoding='
 # Questão 1 - Faça um slicing no dataset para mostrar apenas o país (Country), região (Region), população (Population) e area (Area (sq. mi.)) dos países contidos nele.
 
 print("\n---------- 1 ----------")
-print(dataset[:,0:4])
+print(dataset[0,:4])
 
 # Questão 2 - Conte e em seguida mostre quais são as diferentes Regiões do planeta segundo este dataset.
 
 print("\n---------- 2 ----------")
-regions = np.unique(dataset[1:,1], return_counts=True)
-nomes = regions[0]
-num = len(regions[1])
-print("Quantidade de regioes:", num)
-print("Nomes: ", nomes)
+regioes = np.unique(dataset[1:,1])
+print("Numero de regioes: ", regioes.size)
+print("Regioes: ", regioes)
 
 # Questão 3 - Mostre qual a taxa média de alfabetização (Literacy (%)) do planeta segundo este dataset.
 
 print("\n---------- 3 ----------")
-percentage = np.mean(dataset[1:,9].astype(float))
-print(f"Taxa media de alfabetizacao: {percentage:.2f}%")
+alf = dataset[1:,9].astype(float)
+print(f"Taxa media de alfabetizacao: {alf.mean():.2f}%")
 
 # Questão 4 - Conte quantos países são da América do Norte (NORTHERN AMERICA) segundo este dataset
 
 print("\n---------- 4 ----------")
-locations = dataset[:, 1]
-result = np.char.find(locations, 'NORTHERN AMERICA')
-loc_sum = len(locations[result >= 0])
-print("Paises da America do Norte: ", loc_sum)
+regioes = dataset[1:,1]
+america = regioes[np.char.find(regioes,"NORTHERN AMERICA") >= 0]
+print("Numero de paises da America do Norte: ", america.size)
 
 # Questão 5 - Encontre qual país da América do Sul e Caribe (LATIN AMER. & CARIB) possui a maior renda per capita (GDP ($ per capita)).
 
 print("\n---------- 5 ----------")
-locations = dataset[1:, 1]
-gdp = dataset[1:, 8].astype(float)
-
-result = np.char.find(locations, 'LATIN AMER. & CARIB')
-gdp_amr = gdp[result>=0]
-indice = np.where(gdp==np.max(gdp_amr))[0][0]
-
-print("Pais: ", dataset[indice+1, 0])
-print("Renda per capita: ", np.max(gdp_amr))
+renda = dataset[1:,8].astype(int)
+renda_max = renda[np.char.find(regioes,"LATIN AMER. & CARIB") >= 0].max()
+lista_renda = list(renda)
+print("Pais: ", dataset[:,0][lista_renda.index(renda_max) + 1], "\nRenda: ", renda_max)
